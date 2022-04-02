6a950a68e33e40a9374877952d8ca616edb881e6
arthur teixeira <arthur.silva@globalhitss.com.br>
2022-04-02 14:33:30 -0300

    remove slider

--- page_home.php
+++ page_home.php
@@ -1,11 +1,9 @@
- <?php
+<?php
  /*
 Template Name: PAGINA INICIAL
 */
 ?>
  <?php get_header(); ?>
-
-<?php putRevSlider("inicial") ?>
 
 <h1 class="text-center">Hello World</h1>
 <h1 class="text-center">Hello World</h1>
