c4e3857617346c89af7a11effe156d9a4c084c8b
arthur teixeira <arthur.silva@globalhitss.com.br>
2022-04-02 14:19:02 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+    <!--Footer--><!-- jQuery -->
+        <?php wp_footer(); 
+		wp_enqueue_script("jquery"); ?>  
+ 		<!-- Bootstrap JavaScript -->
+ 		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+ 	</body>
+ </html>
---
+++ page_home.php
@@ -0,0 +1,18 @@
+ <?php
+ /*
+Template Name: PAGINA INICIAL
+*/
+?>
+ <?php get_header(); ?>
+
+<?php putRevSlider("inicial") ?>
+
+<h1 class="text-center">Hello World</h1>
+ 
+ <?php get_footer(); ?>
+
+
+ 
+ 		
+ 
+ 		
